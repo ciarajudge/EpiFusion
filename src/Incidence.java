fbaa4bf4b0ca07e3dc14b27878133ee6843a6f0b
ciarajudge <judge.ciara@gmail.com>
2023-04-20 14:40:30 +0100

    Hardcoded change to initialising array to facilitate using remaster outbreak data, need more permanent fix
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/Incidence.java
+++ src/Incidence.java
@@ -6,7 +6,7 @@ public class Incidence {
     int length;
     public Incidence(String filename) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(filename));
-        incidence = new int [20];
+        incidence = new int [29];
         int i = 0;
         while(scanner.hasNextInt()){
             incidence[i++] = scanner.nextInt();
