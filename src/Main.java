ebbdbfc140150ee03ded615eb5d6825e0f941729
ciarajudge <judge.ciara@gmail.com>
2023-04-20 14:38:34 +0100

    Tested and changed to modelling on the remaster simulated outbreak
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,29 @@
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("EpiFusion");
 
         //Define params
         int numParticles = 10;
 
         //Read in tree
-
+        Tree tree = new Tree("/Users/ciarajudge/Desktop/PhD/EpiFusionData/simulatedtree.txt");
+        //tree.printTree();
 
         //Read in case incidence
-        Incidence caseIncidence = new Incidence("/Users/ciarajudge/Desktop/incidence.txt");
+        Incidence caseIncidence = new Incidence("/Users/ciarajudge/Desktop/PhD/EpiFusionData/weekly_incidence.txt");
         System.out.println(Arrays.toString(caseIncidence.incidence));
 
         //Initialise particles
         Particles particles = new Particles(numParticles, caseIncidence.length);
-        particles.printParticles();
+        //particles.printParticles();
 
         ParticleFilter.particleFilter(caseIncidence, particles);
 
-        particles.saveParticleHistory("/Users/ciarajudge/Desktop/particlehistory.csv");
+        particles.saveParticleHistory("/Users/ciarajudge/Desktop/PhD/EpiFusionResults/particlehistory.csv");
 
     }
 }
