1cdbc718d8e332894836ebd5a7645bfaa6c13d42
ciarajudge <judge.ciara@gmail.com>
2023-05-02 17:36:47 +0100

    Control flow changes
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,30 @@
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("EpiFusion");
 
         //Define params
-        int numParticles = 10;
+        int numParticles = 100;
+        int numIterations = 1000;
 
         //Read in tree
         Tree tree = new Tree("/Users/ciarajudge/Desktop/PhD/EpiFusionData/simulatedtree.txt");
-        //tree.printTree();
+        tree.printTree();
 
         //Read in case incidence
-        Incidence caseIncidence = new Incidence("/Users/ciarajudge/Desktop/PhD/EpiFusionData/weekly_incidence.txt");
-        System.out.println(Arrays.toString(caseIncidence.incidence));
+        Incidence caseIncidence = new Incidence("/Users/ciarajudge/Desktop/PhD/EpiFusionData/incidence.txt");
+        //System.out.println(Arrays.toString(caseIncidence.incidence));
 
-        //Initialise particles
-        Particles particles = new Particles(numParticles, caseIncidence.length);
-        //particles.printParticles();
 
-        ParticleFilter.particleFilter(caseIncidence, particles);
+        ParticleFilter particleFilter = new ParticleFilter(numParticles);
 
-        particles.saveParticleHistory("/Users/ciarajudge/Desktop/PhD/EpiFusionResults/particlehistory.csv");
+        MCMC particleMCMC = new MCMC(particleFilter);
+        particleMCMC.runMCMC(numIterations);
+        double[] finalParams = particleFilter.getParameters();
+
+        //particles.saveParticleHistory("/Users/ciarajudge/Desktop/PhD/EpiFusionResults/particlehistory_poisson.csv");
 
     }
 }
