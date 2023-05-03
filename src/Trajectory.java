384a7185cdb5416196d4b96ce9e5e9fb4835c670
ciarajudge <judge.ciara@gmail.com>
2023-05-03 17:20:31 +0100

    Bundled day into Trajectory
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/Trajectory.java
+++ src/Trajectory.java
@@ -12,3 +12,17 @@ public class Trajectory {
         trajectory.add(day);
     }
 }
+
+class Day {
+    int t;
+    int I;
+    int births;
+    int removals;
+
+    public Day(int t, int I, int births, int removals) {
+        this.t = t;
+        this.I = I;
+        this.births = births;
+        this.removals = removals;
+    }
+}
