6c8e4d31a6da6ea4488ef90c2c5c46e8da2792ed
ciarajudge <judge.ciara@gmail.com>
2023-05-04 14:18:15 +0100

    Day resolution likelihood of a particle given tree events
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/PhyloLikelihood.java
+++ src/PhyloLikelihood.java
@@ -1,49 +1,36 @@
-import java.util.List;
-import java.util.Stack;
+
 
 public class PhyloLikelihood {
-    public static double calculateLikelihood(Tree tree, Particle particle) {
-        int state = particle.state;
-        double likelihood = 1.0;
-        Node root = tree.getRoot();
+    public static double calculateLikelihood(TreeSegment tree, int state, double[] propensities) {
+        int treeBirths = tree.births;
+        int treeSamples = tree.samplings;
+        double conditionalLogP;
 
-        // Traverse the tree in postorder
-        Stack<Node> stack = new Stack<>();
-        stack.push(root);
-        while (!stack.empty()) {
-            Node node = stack.pop();
-            if (node.isLeaf()) {
-                String label = node.getLabel();
-                char stateChar = state.getState(label);
-                double freq = state.getFrequency(stateChar);
-                likelihood *= freq;
-            } else {
-                double branchLength = node.getBranchLength();
-                List<Node> children = node.getChildren();
-                double p = 1.0;
-                for (Node child : children) {
-                    stack.push(child);
-                    p *= computeSubtreeProbability(child, branchLength, state);
-                }
-                likelihood *= p;
+        //Case1 = Likelihood given no events on the tree
+        conditionalLogP = 0-(propensities[4]+ propensities[0]+ propensities[3]);
+        if (treeBirths != 0 || treeSamples != 0) { // Case2 = Likelihood given something does happen
+            //Two types of possible observed events on tree for us: coalescence or sampling. We code them 0, 1
+            for (int i = 0; i < treeBirths; i++) {
+                conditionalLogP += observedEventProbabiltity(0, state, propensities[0] + propensities[1]);
+            }
+            for (int i = 0; i < treeSamples; i++) {
+                conditionalLogP += observedEventProbabiltity(1, state, propensities[4]);
             }
         }
-
-        return likelihood;
+        return conditionalLogP;
     }
 
-    private static double computeSubtreeProbability(Node node, double parentBranchLength, int state) {
-        double branchLength = node.getBranchLength();
-        double t = branchLength / parentBranchLength;
-        char stateChar = state.getState(node.getLabel());
-        double q = state.getSubstitutionProbability(stateChar, t);
-        double p = 1.0;
-        List<Node> children = node.getChildren();
-        for (Node child : children) {
-            p *= computeSubtreeProbability(child, branchLength, state);
+    public static double observedEventProbabiltity(int type, int state, double prop) {
+        double conditionalLogP = 0.0;
+
+        if (type == 0) { //Coalescence
+            conditionalLogP += Math.log(2.0 / state / (state-1) * prop);
         }
-        return q * p;
-    }
+        else {
+            conditionalLogP += Math.log(prop);
+        }
 
+        return conditionalLogP;
+    }
 
 }
