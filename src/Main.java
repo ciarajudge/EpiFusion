55582374e8015368d9fafe12c79b2ce84457a6d7
ciarajudge <judge.ciara@gmail.com>
2023-05-17 16:25:03 +0100

    Big flexibility changes in parameterisation
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,11 @@ public class Main {
         //Define params
         int numParticles = 1;
         int numIterations = 0;
+        Storage.setEpiGrainyResolution();
+        Storage.setPhyloOnly();
+        int resampleEvery = 7;
+        Storage.setResampling(resampleEvery);
+        int T;
 
         //Read in tree
         Tree tree = new Tree("/Users/ciarajudge/Desktop/PhD/EpiFusionData/basesim2_simulatedtree.txt");
@@ -15,17 +20,22 @@ public class Main {
         //Read in case incidence
         Incidence caseIncidence = new Incidence("/Users/ciarajudge/Desktop/PhD/EpiFusionData/basesim2_weeklyincidence.txt");
 
+        //Define more params based on data
+        int epiLength = Storage.isEpiGrainyResolution() ? resampleEvery * caseIncidence.length : caseIncidence.length;
+        double phyloLength = tree.age;
+        T = Math.max(epiLength, (int) Math.round(phyloLength));
+
         //Initialise particle filter instance
-        double[] initialParameters = {0.286,0.143,0.02};
-        ParticleFilter particleFilter = new ParticleFilter(numParticles, initialParameters, tree, caseIncidence);
+        double[] initialParameters = {0.06, -0.043, 0.4, 0.233, 0.007};
+        ParticleFilter particleFilter = new ParticleFilter(numParticles, initialParameters, tree, caseIncidence, T, resampleEvery);
 
         //Initialise and run MCMC instance
         MCMC particleMCMC = new MCMC(particleFilter);
         particleMCMC.runMCMC(numIterations);
 
+        particleFilter.loggers.trajectories.close();
         //Save output
-        double[] finalParams = particleFilter.getCurrentParameters();
-        //particles.saveParticleHistory("/Users/ciarajudge/Desktop/PhD/EpiFusionResults/particlehistory_poisson.csv");
+        //double[][] finalParams = particleFilter.getCurrentParameters();
 
     }
 }
