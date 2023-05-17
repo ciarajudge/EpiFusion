db0260e39fb9a0ec099bb0b8de338d9f33780105
ciarajudge <judge.ciara@gmail.com>
2023-05-17 16:28:12 +0100

    big mess at the moment but needed to push as safety
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/ProcessModel.java
+++ src/ProcessModel.java
@@ -1,14 +1,19 @@
 import org.apache.commons.math3.distribution.PoissonDistribution;
+import java.util.Arrays;
 
 public class ProcessModel {
 
     public static void day(Particle particle, TreeSegment tree, int t, double[] rates) {
         int state = particle.getState();
         double[] propensities = particle.getVanillaPropensities(rates);
+        System.out.println("tree lineages: "+tree.lineages);
+
+        System.out.println("vanilla propensities: "+Arrays.toString(propensities));
         //Divide the propensities into their bits
         double unobservedInfectProp = state > 0
                 ? propensities[0] * (1.0 - tree.lineages * (tree.lineages - 1) / (double) state/(state+1))
                 : 0.0;
+        System.out.println("unobserved infection prop: "+unobservedInfectProp);
 
         double observedInfectProp = propensities[0] - unobservedInfectProp;
         double allowedRecovProp, forbiddenRecovProp;
@@ -20,6 +25,7 @@ public class ProcessModel {
             allowedRecovProp = 0.0;
             forbiddenRecovProp = propensities[1];
         }
+        System.out.println("allowed recov prop: "+allowedRecovProp);
         double sampleProp = propensities[2];
         if (tree.lineages < 1) {
             System.out.println("No tree yet.");
@@ -28,26 +34,35 @@ public class ProcessModel {
         }
         //Calculate the events
         int births = poissonSampler(unobservedInfectProp);
+        System.out.println("births: "+births);
         int deaths = poissonSampler(allowedRecovProp);
+        System.out.println("deaths: "+deaths);
         state = state + births - deaths;
         particle.setState(state);
         if (tree.lineages > 0) {
             double[] adjustedPropensities = new double[]{observedInfectProp, unobservedInfectProp, allowedRecovProp, forbiddenRecovProp, sampleProp};
+            System.out.println("adjusted propensities: "+Arrays.toString(adjustedPropensities));
             double todayPhyloLikelihood = PhyloLikelihood.calculateLikelihood(tree, particle, adjustedPropensities);
-            //System.out.println("Today's Phylo Likelihood: " +todayPhyloLikelihood);
+            System.out.println("Today's Phylo Likelihood: " +todayPhyloLikelihood);
             particle.setPhyloLikelihood(particle.getPhyloLikelihood()+todayPhyloLikelihood);
-            //System.out.println("Overall Phylo Likelihood: "+particle.getPhyloLikelihood());
+            System.out.println("Overall Phylo Likelihood: "+particle.getPhyloLikelihood());
+        } else {
+            particle.setState(particle.getState()+tree.births);
         }
         Day tmpDay = new Day(t, state, births, deaths);
         particle.updateTrajectory(tmpDay);
     }
 
-    public static void week(Particle particle, TreeSegment[] treeSegments, int t, double[] rates) {
-        int weeksToDays = t*7;
-        for (int i=0; i<7; i++) {
-            int actualDay = weeksToDays+i+1;
-            System.out.println("Sending for day "+actualDay+", State currently: "+particle.getState());
-            day(particle, treeSegments[i], actualDay, rates);
+    public static void step(Particle particle, TreeSegment[] treeSegments, int step, double[][] rates) {
+        int t = step*Storage.resampleEvery;
+        int increments = treeSegments.length;
+        for (int i=0; i<increments; i++) {
+            if (Storage.isPhyloOnly() && treeSegments[i].lineages == 0 && step > 1) {
+                break;
+            }
+            int actualDay = t+i;
+            System.out.println("Sending particle "+particle.particleID+" for day "+actualDay+", State currently: "+particle.getState());
+            day(particle, treeSegments[i], actualDay, rates[i]);
         }
     }
 
