e55864f830d109126c9e6210814e2e76d3534174
ciarajudge <judge.ciara@gmail.com>
2023-05-31 16:03:36 +0100

    adding loggers for params and acceptance
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

--- src/Loggers.java
+++ src/Loggers.java
@@ -8,6 +8,8 @@ public class Loggers {
     File folder;
     public FileWriter trajectories;
     public FileWriter likelihoods;
+    public FileWriter params;
+    public FileWriter acceptance;
 
     public Loggers(String filePath) throws IOException {
         folder = new File(filePath);
@@ -21,7 +23,14 @@ public class Loggers {
     public Loggers() throws IOException {
         LocalDateTime currentDateTime = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
-        String folderName = currentDateTime.format(formatter);
+        String folderName;
+        if (Storage.isPhyloOnly()) {
+            folderName = "PhyloOnly_"+Storage.numParticles+"Particles_"+Storage.numMCMCsteps+"Steps_"+currentDateTime.format(formatter);
+        } else if (Storage.isEpiOnly()) {
+            folderName = "EpiOnly_"+Storage.numParticles+"Particles_"+Storage.numMCMCsteps+"Steps_"+currentDateTime.format(formatter);
+        } else {
+            folderName = "Combined_"+Storage.numParticles+"Particles_"+Storage.numMCMCsteps+"Steps_"+currentDateTime.format(formatter);
+        }
         String filePath = "/Users/ciarajudge/Desktop/PhD/EpiFusionResults/"+folderName;
         folder = new File(filePath);
         if (!folder.exists()){
@@ -29,6 +38,8 @@ public class Loggers {
         }
         trajectories = new FileWriter(filePath+"/trajectories.csv");
         likelihoods = new FileWriter(filePath+"/likelihoods.txt");
+        params = new FileWriter(filePath+"/params.txt");
+        acceptance = new FileWriter(filePath+"/acceptance.txt");
     }
 
     public void logTrajectory(Trajectory trajectory) throws IOException {
@@ -46,6 +57,19 @@ public class Loggers {
         likelihoods.write(toWrite);
     }
 
+    public void logParams(Double[] paramSet) throws IOException {
+        String toWrite = "";
+        for (Double param : paramSet) {
+            toWrite = toWrite + param + ",";
+        }
+        toWrite = toWrite + "\n";
+        params.write(toWrite);
+    }
+    public void logAcceptance(int accept) throws IOException {
+        String toWrite = accept + "\n";
+        acceptance.write(toWrite);
+    }
+
     public void logTrajectory(Trajectory trajectory, String acceptance) throws IOException {
         String toWrite = "";
         for (Day d : trajectory.trajectory) {
@@ -59,6 +83,8 @@ public class Loggers {
     public void terminateLoggers() throws IOException {
         trajectories.close();
         likelihoods.close();
+        params.close();
+        acceptance.close();
     }
 
 }
