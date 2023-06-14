5ba70ffe72c1ae043511b04d444075b7fd357896
ciarajudge <judge.ciara@gmail.com>
2023-06-14 16:02:11 +0100

    Better GUI
    Signed-off-by: ciarajudge <judge.ciara@gmail.com>

---
+++ src/EpiFusionGUI.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class EpiFusionGUI {
+    private JPanel panelMain;
+    private JLabel titleLabel;
+    private JLabel fileStemPrompt;
+    private JTextField fileStem;
+    private JLabel betaParamsPrompt;
+    private JButton runButton;
+}
